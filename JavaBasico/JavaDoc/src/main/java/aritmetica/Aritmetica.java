8a109ee7950fa2793145aa5b23a41ae6a0397515
franmauriz <franmaurizpereira@gmail.com>
2020-03-14 21:23:55 +0100

    javadoc

---
+++ JavaBasico/JavaDoc/src/main/java/aritmetica/Aritmetica.java
@@ -0,0 +1,28 @@
+/**
+ * Clase para realizar operaciones aritmeticas
+ */
+package aritmetica;
+
+/**
+ *
+ * @author absol
+ * @version 1
+ * 
+ */
+public class Aritmetica {
+    /**
+     * Operandos para realizar las operaciones
+     */
+    private int a;
+    private int b;
+    
+    /**
+     * 
+     * @param a
+     * @param b
+     * @return El resultado de la suma de a + b; 
+     */
+    public static int sumar(int a , int b){
+        return a + b;
+    }
+}
---
+++ JavaBasico/JavaDoc/src/main/java/aritmetica/PruebaAritmetica.java
@@ -0,0 +1,11 @@
+package aritmetica;
+
+/**
+ *
+ * @author absol
+ */
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        System.out.println(Aritmetica.sumar(3, 5));
+    }
+}
