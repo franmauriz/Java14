194ceac99db7bc304780cce932c9a367d972748c
franmauriz <franmaurizpereira@gmail.com>
2020-04-12 16:28:09 +0200

    Obtener los valores de un formulario a traves de un Servlet

---
+++ Servlets_JSPs/ManejoFormularioHTML/src/main/java/web/Servlet.java
@@ -0,0 +1,113 @@
+package web;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        String tecnologias[] = request.getParameterValues("tecnologia");
+        String genero = request.getParameter("genero");
+        String ocupacion = request.getParameter("ocupacion");
+        String musicas[] = request.getParameterValues("musica");
+        String comentarios = request.getParameter("comentarios");
+        
+        out.print("<h1>Resultado del Formulario</h1><hr/>");
+        
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>Datos Formualrio</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<table border=1");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Usuario");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(usuario);
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Password");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(password);
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Tecnologias");
+        out.print("</td>");
+        out.print("<td>");
+            out.print("<ul>");
+            for(String tecnologia:tecnologias){
+                out.print("<li>" + tecnologia+ "</li>");
+            }
+            out.print("</ul>");        
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Genero");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(genero);
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Ocupacion");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(ocupacion);
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Música");
+        out.print("</td>");
+        out.print("<td>");
+            out.print("<ul>");
+            for(String musica:musicas){
+                out.print("<li>" + musica + "</li>");
+            }
+            out.print("</ul>");        
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Comentarios");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(comentarios);
+        out.print("</td>");
+        out.print("</tr>");
+        
+        out.print("</table>");
+        out.print("</body>");
+        out.print("</html>");
+        
+        
+        
+    }
+}
